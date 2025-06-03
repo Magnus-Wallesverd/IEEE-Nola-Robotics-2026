9442b37313566f5d131bd4b17bf5c17c8aa5be15
U-DESKTOP-QIC4HGP\Paige <pwallesverd@gmail.com>
2025-06-03 08:05:18 -0500

    changed blink1.c to main.c for develop branch and cleared the file

---
+++ stm32flash/src/main.c
@@ -0,0 +1,14 @@
+/*
+ * This is the main function for the develop branch
+ * It will be left blank so any person wanting to branch from develop
+ * Any main.c programs made on different branches should be left in its own repo
+ * 
+ * Again... Do not pull request to develop with a main.c that has changes until agreed on
+ * */
+
+#include "stm32f303.h"
+
+
+void main(void){
+    
+}
