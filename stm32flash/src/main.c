ddd65094f1df2c49ddb2fbbbaaf8d0f8ff8fc7e1
Paige Wallesverd <pwallesverd@gmail.com>
2025-06-05 15:48:50 -0500

    changes to commit so i can branch

--- stm32flash/src/main.c
+++ stm32flash/src/main.c
@@ -8,42 +8,41 @@
 
 #include "stm32f303.h"
 
-
 void main(void){
     /* Reset the RCC clock configuration to the default reset state ------------*/
     /* Set HSION bit */
-    RCC->CR |= (uint32_t)0x00000001;
+    //RCC->CR |= (uint32_t)0x00000001;
 
     /* Reset CFGR register */
-    RCC->CFGR &= 0xF87FC00C;
+    //RCC->CFGR &= 0xF87FC00C;
 
     /* Reset HSEON, CSSON and PLLON bits */
-    RCC->CR &= (uint32_t)0xFEF6FFFF;
+    //RCC->CR &= (uint32_t)0xFEF6FFFF;
 
     /* Reset HSEBYP bit */
-    RCC->CR &= (uint32_t)0xFFFBFFFF;
+    //RCC->CR &= (uint32_t)0xFFFBFFFF;
 
     /* Reset PLLSRC, PLLXTPRE, PLLMUL and USBPRE bits */
-    RCC->CFGR &= (uint32_t)0xFF80FFFF;
+    //RCC->CFGR &= (uint32_t)0xFF80FFFF;
 
     /* Reset PREDIV1[3:0] bits */
-    RCC->CFGR2 &= (uint32_t)0xFFFFFFF0;
+    //RCC->CFGR2 &= (uint32_t)0xFFFFFFF0;
 
     /* Reset USARTSW[1:0], I2CSW and TIMs bits */
-    RCC->CFGR3 &= (uint32_t)0xFF00FCCC;
+    //RCC->CFGR3 &= (uint32_t)0xFF00FCCC;
 
     /* Disable all interrupts */
-    RCC->CIR = 0x00000000;
+    //RCC->CIR = 0x00000000;
     // for(volatile int i = 0; i < 10000; i++);
     
     RCC->AHBENR |= 0x20000;
      /* RCC->APB1ENR |= 1;
      */
-    SetPinOutput(GPIOA,0xffff);
+    SetPinOutput(GPIOA,0x20);
     /*AlternateFunctionSet(GPIOA,0x03,1);
     InitGenTIM(TIM2);
     */
-    PinWrite(GPIOA,0xffff);
+    PinWrite(GPIOA,0x20);
     while(1){
         for(volatile int i = 0; i < 10000; i++);
     }
