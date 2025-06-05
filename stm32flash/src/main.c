a700d51052dbd54862fe9bcfbf522235acb85a97
Paige Wallesverd <pwallesverd@gmail.com>
2025-06-04 22:21:42 -0500

    broke

--- stm32flash/src/main.c
+++ stm32flash/src/main.c
@@ -1,4 +1,4 @@
-/*
+ /*
  * This is the main function for the develop branch
  * It will be left blank so any person wanting to branch from develop
  * Any main.c programs made on different branches should be left in its own repo
@@ -10,5 +10,17 @@
 
 
 void main(void){
-    
-}
+
+    for(volatile int i = 0; i < 10000; i++);
+    /*RCC->AHBENR |= 0x20000;
+     * RCC->APB1ENR |= 1;
+     */
+    //SetPinOutput(GPIOA,0xffff);
+    /*AlternateFunctionSet(GPIOA,0x03,1);
+    InitGenTIM(TIM2);
+    PinWrite(GPIOA,0xffff);
+    */
+    while(1){
+        for(volatile int i = 0; i < 10000; i++);
+    }
+ }
