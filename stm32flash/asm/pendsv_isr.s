4931ac6ef6667a272707fd13c87119e55b55b82b
U-DESKTOP-QIC4HGP\Paige <pwallesverd@gmail.com>
2025-06-29 03:02:48 -0500

    comments

--- stm32flash/asm/pendsv_isr.s
+++ stm32flash/asm/pendsv_isr.s
@@ -4,6 +4,7 @@
 /* calls the task scheduler for context switching information */
 
 PendSV_Handler:
+   
     /* set current to next */
     ldr r0, =next_tcb
     ldr r0, [r0]
@@ -18,6 +19,8 @@ PendSV_Handler:
     ldr r3, [r2]
     msr psp, r3
     ISB
+    
+    /* Exception return */
     ldr r0, =0xFFFFFFFD
     bx r0
 
