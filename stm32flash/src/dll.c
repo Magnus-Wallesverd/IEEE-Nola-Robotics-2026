2c94354b2b1efe7d0028c43058e3d780f69ca1fe
Paige Wallesverd <pwallesverd@gmail.com>
2025-07-22 14:04:32 -0500

    still on malloc

--- stm32flash/src/dll.c
+++ stm32flash/src/dll.c
@@ -1,22 +1,43 @@
 #include "stm32f303.h"
+#include <stdint.h>
 
 node_t heap[DLL_SIZE];
 uint32_t memory_bmp1 = 0;
 uint32_t memory_bmp2 = 0;
+uint32_t* bmp1_ptr= 0;
+uint32_t* bmp2_ptr= 0;
 
 void add_next(void* head, void* ptr){
+    if(~memory_bmp1 != 0){
+
+    } else if(~memory_bmp2 != 0){
+
+    } else { return; }
 
 }
 
 void rm_next(void* head, void* ptr){
+    if(~memory_bmp1 != 0){
 
+    } else if(~memory_bmp2 != 0){
+
+    } else { return; }
 }
 
 void add_prev(void* head, void* ptr){
+    if(~memory_bmp1 != 0){
 
+    } else if(~memory_bmp2 != 0){
+
+    } else { return; }
 }
 
 void rm_prev(void* head, void* ptr){
+    if(~memory_bmp1 != 0){
+
+    } else if(~memory_bmp2 != 0){
+
+    } else { return; }
 
 }
 
@@ -24,9 +45,9 @@ void* malloc(void* ptr){
     if(~memory_bmp1 != 0){
         
     }
-    return ptr;   
+    return ptr; 
 }
 
-void free(uint32_t bmp){
-    
+void free(uint32_t i, uint32_t* bmp_ptr){
+    *bmp_ptr ^= (1 << i);
 }
