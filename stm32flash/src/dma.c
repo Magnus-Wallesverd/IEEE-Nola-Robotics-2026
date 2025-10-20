9e8c84507d592c54eb6a8b4d2def053eb1f1e08f
Paige Wallesverd <pwallesverd@gmail.com>
2025-10-20 15:00:14 -0500

    finish this pls

--- stm32flash/src/dma.c
+++ stm32flash/src/dma.c
@@ -8,13 +8,14 @@ void configure_spi(SPI_TypeDef* SPIx){
     switch((uint32_t)SPIx){
         case (uint32_t)SPI1:
             DMA->CPAR2 |= (uint32_t)&SPI1->DR;
-            // DMA->CMAR2 |= ; // make a buffer for this somewhere
+            DMA->CMAR2 |= ; // make a buffer for this somewhere
             DMA->CPAR3 |= (uint32_t)&SPI1->DR;
-            // DMA->CMAR3 |= ; // make a buffer for this somewhere
+            DMA->CMAR3 |= ; // make a buffer for this somewhere
             DMA->CNDTR2 |= 1;
             DMA->CNDTR3 |= 1;
             DMA->CCR2  |= 0x0A82;       // Rx
             DMA->CCR3  |= 0x0A92;       // Tx
+            // dont forget enable disable
             break;
         case (uint32_t)SPI2:
             break;
@@ -24,5 +25,3 @@ void configure_spi(SPI_TypeDef* SPIx){
             break;
     }
 }
-
-void dma
