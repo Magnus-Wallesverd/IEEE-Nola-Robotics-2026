b0b9f7952fcd54b37c16a4984f1e7577c20cb11a
Paige Wallesverd <pwallesverd@gmail.com>
2026-03-09 14:28:07 -0500

    fixed in one line

--- stm32flash/src/usart.c
+++ stm32flash/src/usart.c
@@ -24,6 +24,7 @@ void USART1_IRQHandler(void){
     
     if((USART1->ISR & USART_RXNE) && (USART1->CR1 & USART_RXNEIE)){
         usart.rx_buffer_p[rx_i++%USART_BUF_SIZE] = usart.USARTx->RDR;
+        rx_i -= (usart_rx_buffer[0] >> 6 == 0b10);
         int8_t theta = usart_rx_buffer[0];
         target = usart_rx_buffer[1];
         target_h = (*get_i2c_buffer()|*(get_i2c_buffer()+1)<<8) + (theta*16);
