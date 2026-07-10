4d4e0db7c1b68d1732e9e98942794779ebaaa353
Paige Wallesverd <pwallesverd@gmail.com>
2026-07-10 17:27:56 -0500

    sequencer start

---
+++ stm32flash/src/euler_rhythm.c
@@ -0,0 +1,61 @@
+#include <stdint.h>
+
+
+void rotate(int* array, int size, int shift){
+    
+    int temp;
+
+    for(int i = 0; i < shift; ++i){
+        for(int j = 0; j < size-1; ++j){
+            temp = array[j+1];
+            array[j+1] = array[j];
+            array[j] = temp;
+        }
+    }
+}
+
+void euler_rhythm(int* array, int total_beats, int hits){
+
+    int sum = 0;
+
+    for(int i = 0; i < total_beats; ++i){
+        sum+=hits;
+        if(sum >= total_beats){
+            array[i] = 1;
+            sum-=total_beats;
+        } else {
+            array[i] = 0;
+        }
+    }
+
+    rotate(array, total_beats, total_beats-1);
+}
+
+void print_rhythm(int* array, int size){
+
+    printf("[ ");
+
+    for(int i = 0; i < size; ++i){
+        printf("%d ", array[i]);
+    }
+
+    printf("]\n");
+
+}
+
+int euler_main()
+{
+    int rhythm[32] = {0};
+    int total_beats = *argv[1]-0x30;
+    int hits = *argv[2]-0x30;
+    int shift = *argv[3] - 0x30;
+
+    euler_rhythm(rhythm, total_beats,  hits);
+    
+    print_rhythm(rhythm, total_beats);
+
+    // rotate(rhythm, total_beats, shift);
+
+
+    return 0;
+}
